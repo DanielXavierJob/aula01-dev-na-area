8543a12f431b57bb5c3cbe1d52f18e776733958b
Daniel Xavier <100311104+DanielXavierJob@users.noreply.github.com>
2022-11-26 20:51:43 -0300

    🎉 init: initializing aula 02

--- Resources/Application/Auth/index.tsx
+++ Resources/Application/Auth/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Head from "next/head"
 import Image from 'next/image'
 import FooterPage from '../../Components/Footer'
-import { ToastContainer } from 'react-nextjs-toast'
 
 type AuthPage = {
   pageTitle: string,
